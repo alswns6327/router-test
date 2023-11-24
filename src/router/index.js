26c6b46b4f2b97fbc09c3cd6961392402dd2c179
owner <alswns6327@naver.com>
2023-11-24 13:31:03 +0900

    전역 navigation guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, isNavigationFailure } from 'vue-router';
 
 import Home from '../pages/Home.vue';
 import About from '../pages/About.vue';
@@ -6,6 +6,12 @@ import Members from '../pages/Members.vue';
 import MemberInfo from '../pages/MemberInfo.vue';
 import Videos from '../pages/Videos.vue';
 import ViedoPlayer from '../pages/VideoPlayer.vue';
+
+const memberInfo = (to, from)=>{
+    if(to.from !== 'members' && to.from !== "members/id"){
+        return false;
+    }
+}
 
 const router = createRouter({
     history : createWebHistory(),
@@ -15,12 +21,25 @@ const router = createRouter({
         {path: '/members', name:'members' , component: Members},
         {path: '/members/:id(\\d+)+', name:'memberInfo' , component: MemberInfo},
         {
-            path: '/songs', name:'videos' , component: Videos,
+            path: '/videos', name:'videos' , component: Videos,
             children: [
                 {path: ':id', name:'videos/id', component: ViedoPlayer}
             ]
         },
     ]
+});
+
+router.beforeEach((to, from) =>{
+    // to.query에 속성이 존재할 경우 제거한 뒤 이동
+    if(to.query && Object.keys(to.query).length > 0){
+        return {path:to.path, query:{}, params:to.params}
+    }
+});
+
+router.afterEach((to, from, failure) => {
+    if(isNavigationFailure(failure)){
+        console.log("@@내비게이션 중단 : ", failure);
+    }
 });
 
 export default router;
